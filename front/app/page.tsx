873e8fe71a83b68783b6a22eb9e525e7d2d479d8
Jérôme Roux <jerome.roux@pole-emploi.fr>
2024-02-11 11:22:18 +0100

    feat(front): ajoute un bouton de recherche

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -71,6 +71,13 @@ export default function Home() {
                                                placeholder=" "/>
                                         <label htmlFor="club"
                                                className="absolute top-0 p-4 origin-0 duration-300 -z-1 bg-white text-gray-500">Saisie ton club</label>
+                                        <button type="submit" className="absolute right-6 top-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                                                 stroke="currentColor" className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round"
+                                                      d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
+                                            </svg>
+                                        </button>
                                     </div>
                                 </div>
                             </div>
