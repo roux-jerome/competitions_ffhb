122f797f4fbeef26fd58d770a29546ebc23663d1
Jérôme Roux <jerome.roux@pole-emploi.fr>
2024-02-13 09:53:09 +0100

    build(front): supprime speedinsight qui retourne une erreur

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,7 +1,6 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
-import {SpeedInsights} from "@vercel/speed-insights/next"
 import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({subsets: ["latin"]});
@@ -18,7 +17,6 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <SpeedInsights/>
         <Analytics/>
         <body className={inter.className}>{children}</body>
         </html>
