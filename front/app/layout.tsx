af823cddd8487f12ad4ed25d776f6d90cb48ee22
Jérôme Roux <jerome.roux@pole-emploi.fr>
2024-02-23 00:13:20 +0100

    feat(feedback): mise en place du formulaire de feedback

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,13 +1,15 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
-import { Analytics } from '@vercel/analytics/react';
+import {Analytics} from '@vercel/analytics/react';
+import Script from "next/script";
 
 const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
     title: "Recherche FFHB",
     description: "Recherche ton équipe sur le site de la Fédération Française de handball ",
+    other: {"userreport:mediaId": "adb3fa7a-514b-48bb-b0b6-bafd45bac495"}
 };
 
 export default function RootLayout({
@@ -17,6 +19,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+        <Script src="https://sak.userreport.com/pers1/launcher.js" async id="userreport-launcher-script"></Script>
         <Analytics/>
         <body className={inter.className}>
         {children}
