afb7b07d7e6de7e57d390c4ac62af972f110f485
Jérôme Roux <jerome.roux@pole-emploi.fr>
2024-02-22 10:10:44 +0100

    fix(match): corrige l'alignement en grand écran

--- front/app/ui/matchs/liste-matchs.tsx
+++ front/app/ui/matchs/liste-matchs.tsx
@@ -4,10 +4,10 @@ export function ListeMatchs({matchs}: { matchs: Match[] }) {
     return matchs.map(match =>
 
         <a key={`${match.rencontre.equipe1Libelle}-${match.rencontre.equipe2Libelle}`}
-           className="flex items-center justify-between px-2 hover:bg-blue-100 duration-300 rounded-lg transition-colors"
+           className="flex items-center justify-between px-1 hover:bg-blue-100 duration-300 hover:rounded transition-colors pb-4 mb-1 border-b-2 bt-6 "
            href={"https://www.ffhandball.fr/competitions/saison-2023-2024-19/" + match.url} target="_blank"
         >
-            <div className="pt-6 pb-2 border-b-2 grid grid-cols-2">
+            <div className="grid grid-cols-2 w-full">
                 <div>
                     <span className="block font-bold">{match.categorie}{match.type}</span>
                     <small className="block text-xs text-gray-500">{match.estADomicile ? match.rencontre.equipe1Libelle : match.rencontre.equipe2Libelle}</small>
@@ -22,7 +22,7 @@ export function ListeMatchs({matchs}: { matchs: Match[] }) {
                 </span>
                 </div>
             </div>
-            <svg className="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+            <svg className="w-3 h-3 rtl:rotate-180 mt-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
             </svg>
         </a>
