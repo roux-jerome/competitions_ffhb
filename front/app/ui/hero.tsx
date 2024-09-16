b7b871c36f472a372f7e04e43d05f90cce627616
Jérôme Roux <jerome.roux@pole-emploi.fr>
2024-09-16 20:52:01 +0200

    feat(hero): correction mineur

--- front/app/ui/hero.tsx
+++ front/app/ui/hero.tsx
@@ -2,7 +2,7 @@
 import {motion} from "framer-motion";
 import {HeroHighlight, Highlight} from "@/app/ui/hero-highlight";
 
-export function Hero(){
+export function Hero() {
 
     return <HeroHighlight>
         <motion.h1
@@ -20,14 +20,15 @@ export function Hero(){
             }}
             className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
         >
-            Avec RechercheFFHB, recherche les informations sur ton club de handball. {"     "}
+            Avec RechercheFFHB, trouve les informations sur tes clubs de handball préférés. <br/>
             <Highlight className="text-black dark:text-white">
                 Simple, rapide et efficace
             </Highlight>
 
         </motion.h1>
-        <p className="mt-10 md:mt-20 mx-10 md:text-2xl font-bold text-gray-500 dark:text-gray-400">Toi aussi tu n&apos;en peux plus de rechercher sur le site de la Fédération Française de
-            Handball. <span
-                className="text-orange-600">Alors cet outil est fait pour toi !</span></p>
+        <p className="mt-10 md:mt-20 mx-10 md:text-2xl font-bold text-gray-500 dark:text-gray-400">
+            Toi aussi tu n&apos;en peux plus de rechercher sur le site de la Fédération
+            Française de Handball alors { ' '}
+            <span className="text-orange-600">cet outil est fait pour toi !</span></p>
     </HeroHighlight>
 }
