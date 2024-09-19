b3366416217867c105d288380b4acf172431136a
Jérôme Roux <jerome.roux@pole-emploi.fr>
2024-09-20 00:36:06 +0200

    feat(design): revois l'entete mobile

--- front/app/ui/entete/entete.tsx
+++ front/app/ui/entete/entete.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export function Entete() {
     return <header className="bg-white dark:bg-gray-900">
-        <nav className="bg-gradient-to-r from-orange-700 to-orange-100 pt-1">
+        <nav className="bg-gradient-to-r from-orange-700 to-orange-300 pt-1">
             <div className="bg-white w-full">
                 <div className="container flex items-center justify-between px-6 py-3 mx-auto w-full">
                     <Image className="w-auto h-10" src={iconimageHand} width={600} height={80} alt=""/>
